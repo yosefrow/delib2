e83e3c9ef9d7b1ee6a5db76b23eec6fe916256cd
Tal Yaron <tal.yaron@gmail.com>
2016-05-26 17:11:48 +0300

    Move to topics

--- js/group.js
+++ js/group.js
@@ -1,41 +1,58 @@
 function showGroup(groupUid){
-  console.log(groupUid);
 
   DB.child("groups/"+groupUid).once("value", function(dataSnapshot){
     var title = dataSnapshot.val().title;
      convertTemplate("#headerTitle-tmpl", {group: title}, "#headerTitle");
   });
+
+  showGroupTopics (groupUid);
 }
 
 //show group topics
 function showGroupTopics(groupUid){
   //get group topics
-  DB.child("groups/"+groupUid).on("value",function(topics){
-    var topicsArray = new Array();
-    var topicsUnderGroup = topics.val();
-    var numberOfTopics = Object.keys(topicsUnderGroup).length;
 
-    var i = 1;
-    topics.forEach(function(topic){
-      var preContext = new Object();
-      DB.child("topics/"+topic).once(function(data){
-        var title = data.val().title;
-        var description = data.val().description;
+  DB.child("groups/"+ groupUid.toString()+"/topics").on("value",function(topics){
 
-        preContext = {
-          uuid: topic.key,
-          title: title,
-          description: description
-        }
+    if(topics.exists()){
 
-        topicsArray.push(preContext);
+      var topicsUnderGroup = topics.val();
+      var numberOfTopics = Object.keys(topicsUnderGroup).length;
+      var topicsArray = new Array();
 
-        if (i === numberOfTopics){
-            var context = {groups: groupsArray};
-            convertTemplate("#groups_"+role+"-tmpl", context, "wrapper");
+      var i = 1;
+
+      topics.forEach(function(topic){
+
+        DB.child("topics/"+topic.key).once("value", function(data){
+
+          var preContext = new Object();
+
+          if (data.exists()){
+
+            var title = data.val().title;
+            var description = data.val().description;
+  //          console.log("t: "+ title + ", d: "+ description);
+
+            preContext = {
+              uuid: topic.key,
+              title: title,
+              description: description
+            }
+
+            topicsArray.push(preContext);
           }
+          console.log("i = "+i+", numberOfTopics = "+ numberOfTopics);
+          if (i === numberOfTopics){
+              var context = {groups: topicsArray};
+              convertTemplate("#groupPage-tmpl", context, "wrapper");
+            console.log("Push"+ JSON.stringify(context));
+          }
+          console.log(i);
           i++;
+        })
+
       })
-    })
+    } else {convertTemplate("#groupPage-tmpl",{}, "wrapper");}
   });
 }
