d6130634023ad514f75c638c2304a4bf6decb636
Tal Yaron <tal.yaron@gmail.com>
2016-06-05 08:35:51 +0300

    just indenting

--- js/login.js
+++ js/login.js
@@ -19,83 +19,87 @@
 
 var user = firebase.auth().currentUser;
 if (user) {
- // User is signed in.
-    convertTemplate("#LogoHeaderTitle-tmpl",{},"#headerTitle");
-    convertTemplate("#headerBreadCrumbs-tmpl",{},"headerBreadCrumbs");
-    convertTemplate("#headerMenu-tmpl",{},"headerMenu");
+  // User is signed in.
+  convertTemplate("#LogoHeaderTitle-tmpl",{},"#headerTitle");
+  convertTemplate("#headerBreadCrumbs-tmpl",{},"headerBreadCrumbs");
+  convertTemplate("#headerMenu-tmpl",{},"headerMenu");
 
 } else {
- // No user is signed in.
+  // No user is signed in.
 
 }
 
-      var DB = firebase.database().ref();
-      var storage = firebase.storage();
-      //Google login
-      function googleLogin() {
-        var provider2 = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithRedirect(provider2);
-      }
-      //Facebook login
-      function facebookLogin(){
-        var provider = new firebase.auth.FacebookAuthProvider();
-        firebase.auth().signInWithRedirect(provider);
-      }
+//var DB = firebase.database().ref();
+//var storage = firebase.storage();
+//Google login
+function googleLogin() {
+  var provider2 = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithRedirect(provider2);
+}
+//Facebook login
+function facebookLogin(){
+  var provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithRedirect(provider);
+}
 
-      firebase.auth().getRedirectResult().then(function(result) {
-          if (result.credential) {
-            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-            var token = result.credential.accessToken;
-            var userName = result.credential.user;
-            console.log("token: "+ token);
-            // ...
-          }
-          // The sig  ned-in user info.
-          console.dir(result.user);
-          console.dir(result);
-        //        window.location = "";
-        }).catch(function(error) {
-          // Handle Errors here.
-          console.log(error);
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          console.log("FB error: " + email+ ", "+errorMessage + ", "+errorCode + ", "+ credential);
-          // ...
-        });
-      $("#signup-form").submit(function(e){
-        e.preventDefault();
-        console.log(this.email.value+ ", "+ this.pass.value);
-        firebase.auth().createUserWithEmailAndPassword(this.email.value, this.pass.value).catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          console.log("Error: "+ errorMessage+" : "+ errorCode);
-          });
-      });
-      $("#login-form").submit(function(e){
-        e.preventDefault();
-        firebase.auth().signInWithEmailAndPassword(this.email.value, this.pass.value).catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          console.log("Error: "+ errorMessage+" : "+ errorCode);
-        });
-      });
-      function signout(){
-        firebase.auth().signOut().then(function() {
-          console.log("sign out");
-        }, function(error) {
-          console.log("Error: "+ error)
-        });
-      }
-      firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          console.log("User: "+ user.email +", "+ user.uid);
-        } else {
-          console.log("No user is signed in.");
-        }
-      });
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    var userName = result.credential.user;
+    console.log("token: "+ token);
+    // ...
+  }
+  // The sig  ned-in user info.
+  console.dir(result.user);
+  console.dir(result);
+  //        window.location = "";
+}).catch(function(error) {
+  // Handle Errors here.
+  console.log(error);
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  console.log("FB error: " + email+ ", "+errorMessage + ", "+errorCode + ", "+ credential);
+  // ...
+});
+
+$("#signup-form").submit(function(e){
+  e.preventDefault();
+  console.log(this.email.value+ ", "+ this.pass.value);
+  firebase.auth().createUserWithEmailAndPassword(this.email.value, this.pass.value).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log("Error: "+ errorMessage+" : "+ errorCode);
+  });
+});
+
+$("#login-form").submit(function(e){
+  e.preventDefault();
+  firebase.auth().signInWithEmailAndPassword(this.email.value, this.pass.value).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log("Error: "+ errorMessage+" : "+ errorCode);
+  });
+});
+
+function signout(){
+  firebase.auth().signOut().then(function() {
+    console.log("sign out");
+  }, function(error) {
+    console.log("Error: "+ error)
+  });
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log("Loged in, User: "+ user.email +", "+ user.uid);
+  } else {
+    console.log("Signed out");
+  }
+});
