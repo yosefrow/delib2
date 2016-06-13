fd0ee8fac63cd88476bdb3ef63b81025cf9914e6
Tal Yaron <tal.yaron@gmail.com>
2016-06-13 10:39:45 +0300

    chat 01

--- js/chat.js
+++ js/chat.js
@@ -4,45 +4,12 @@ function showChat(chatUid){
 
 
 
+function clearChat(){
+  $("wrapper").html("");
+}
 function showChat(chatUid){
 
-  //show 20 existing messages
-  DB.child("chats/"+chatUid).orderByChild("time").limitToLast(20).on("value", function(chats){
-
-    $("wrapper").html("<div id='messages'></div>");
-    if (chats.exists()){
-      chats.forEach(function(chat){
-
-        var text = chat.val().text;
-        var time =  parseDate(chat.val().time);
-        var author = chat.val().user;
-
-        DB.child("users/"+author).once("value", function(dataSnapshot){
-          if (dataSnapshot.exists()){
-            var user = new Object;
-            user.firstName = dataSnapshot.val().first_name;
-            user.lastName = dataSnapshot.val().last_name;
-            var context = {text:text, time: time, userName: user.firstName, userLast: user.lastName, messageId: chatUid}
-
-            appendTemplate("#chatMessage-tmpl", context, "#messages");
-//            var messagesDiv = document.getElementById("messages");
-//            var messagesHeight = $("#messages").height();
-//            console.log(messagesHeight);
-////            messagesDiv.scrollTop = messagesHeight;
-//            $("#messages").scrollTop(500);
-            $('wrapper').scrollTop($('wrapper')[0].scrollHeight);
-          } else {
-            $("wrapper").html("<div id='messages'></div>");
-          }
-        })
-      })
-//      $("wrapper").animate({ scrollTop: $('wrapper').prop("scrollHeight")}, 1000);
-    }
-    //    console.log("hoop: "+ chats.val().text);
-    //    console.dir(chats.val());
-  })
-
-
+  clearChat();
 
   convertTemplate("#chatInput-tmpl",{},"footer");
 
@@ -57,8 +24,77 @@ function showChat(chatUid){
       $("#chatInputTxt").val("");
     }
   });
+  DB.child("chats/"+chatUid).off();
+  DB.child("chats/"+chatUid).orderByChild("time").limitToLast(20).on("child_added", function(chats){
+    if(chats.exists()){
 
+      var text = chats.val().text;
+      var time =  parseDate(chats.val().time);
+      var author = chats.val().user;
+      console.log(text, time, author);
+
+      var context = {text:text, time: time, author:author};
+      appendTemplate("#chatMessage-tmpl", context, "wrapper");
+
+      $('wrapper').scrollTop($('wrapper')[0].scrollHeight);
+    }
+  })
 }
+
+
+//  //show 20 existing messages
+//  DB.child("chats/"+chatUid).orderByChild("time").limitToLast(20).on("value", function(chats){
+//
+//    $("wrapper").html("<div id='messages'></div>");
+//    if (chats.exists()){
+//      chats.forEach(function(chat){
+//
+//        var text = chat.val().text;
+//        var time =  parseDate(chat.val().time);
+//        var author = chat.val().user;
+//
+//        DB.child("users/"+author).once("value", function(dataSnapshot){
+//          if (dataSnapshot.exists()){
+//            var user = new Object;
+//            user.firstName = dataSnapshot.val().first_name;
+//            user.lastName = dataSnapshot.val().last_name;
+//            var context = {text:text, time: time, userName: user.firstName, userLast: user.lastName, messageId: chatUid}
+//
+//            appendTemplate("#chatMessage-tmpl", context, "#messages");
+//            //            var messagesDiv = document.getElementById("messages");
+//            //            var messagesHeight = $("#messages").height();
+//            //            console.log(messagesHeight);
+//            ////            messagesDiv.scrollTop = messagesHeight;
+//            //            $("#messages").scrollTop(500);
+//            $('wrapper').scrollTop($('wrapper')[0].scrollHeight);
+//          } else {
+//            $("wrapper").html("<div id='messages'></div>");
+//          }
+//        })
+//      })
+//      //      $("wrapper").animate({ scrollTop: $('wrapper').prop("scrollHeight")}, 1000);
+//    }
+//    //    console.log("hoop: "+ chats.val().text);
+//    //    console.dir(chats.val());
+//  })
+//
+//
+//
+//  convertTemplate("#chatInput-tmpl",{},"footer");
+//
+//  //listen to enter from input
+//  $("#chatInputTxt").keypress(function (e) {
+//    if (e.keyCode == 13) {
+//      e.preventDefault();
+//      console.log("enter!")
+//      var inputValue=$("#chatInputTxt").val();
+//      console.log(inputValue);
+//      addChatMessage(chatUid, userUuid, inputValue);
+//      $("#chatInputTxt").val("");
+//    }
+//  });
+//
+//}
 
 function addChatMessage(chatUid, userUid, text){
   //  var x= firebase.database(app);
