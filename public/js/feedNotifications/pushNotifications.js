f76e6a4726c6fb43990ddc190009a901ef587537
Tal Yaron <tal.yaron@gmail.com>
2016-07-04 21:26:50 +0300

    login bug fix

--- public/js/feedNotifications/pushNotifications.js
+++ public/js/feedNotifications/pushNotifications.js
@@ -7,40 +7,40 @@ var subFcm;
 // Request notifications permission on page load
 
 document.addEventListener('DOMContentLoaded', function () {
-    if (Notification.permission !== "granted")
-        Notification.requestPermission();
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
 });
 
 
 //------External FCM Notifications-------
 
 function sefFcmPushNotiifications() {
- if (isSubscribed) {
-   fcmUnsubscribe();
- } else {
-   fcmSubscribe();
- }
+  if (isSubscribed) {
+    fcmUnsubscribe();
+  } else {
+    fcmSubscribe();
+  }
 }
 
 function fcmSubscribe() {
-    SWreg.pushManager.subscribe({userVisibleOnly: true}).
-    then(function(pushSubscription) {
-        subFcm = pushSubscription;
-        console.log('Subscribed! Endpoint:', subFcm.endpoint);
-        $('#fcmPushNotiificationsBtn').prop('value', 'Unsubscribe');
-        isSubscribed = true;
-    });
+  SWreg.pushManager.subscribe({userVisibleOnly: true}).
+  then(function(pushSubscription) {
+    subFcm = pushSubscription;
+    console.log('Subscribed! Endpoint:', subFcm.endpoint);
+    $('#fcmPushNotiificationsBtn').prop('value', 'Unsubscribe');
+    isSubscribed = true;
+  });
 }
 
 function fcmUnsubscribe() {
-    subFcm.unsubscribe().then(function(event) {
-        $('#fcmPushNotiificationsBtn').prop('value', 'Subscribe');
-        console.log('Unsubscribed!', event);
-        isSubscribed = false;
-    }).catch(function(error) {
-        console.log('Error unsubscribing', error);
-        $('#fcmPushNotiificationsBtn').prop('value', 'Subscribe');
-    });
+  subFcm.unsubscribe().then(function(event) {
+    $('#fcmPushNotiificationsBtn').prop('value', 'Subscribe');
+    console.log('Unsubscribed!', event);
+    isSubscribed = false;
+  }).catch(function(error) {
+    console.log('Error unsubscribing', error);
+    $('#fcmPushNotiificationsBtn').prop('value', 'Subscribe');
+  });
 }
 
 
@@ -48,71 +48,72 @@ function fcmUnsubscribe() {
 
 
 function setGlobalNotifications() {
-    
-    if(activeEntity !== 'undefined') {
+  console.log("setGlobalNotifications");
 
-        // console.dir(userEntityNotificationsExists);
-        
-        var GlobalNotifications = DB.child("users/"+userUuid+"/entityNotifications/"+activeEntity.entity+"/"+activeEntity.uid+"/globalNotifications");
-            
-        if (userEntityNotificationsExists)
-        {
+  if(activeEntity !== 'undefined') {
 
-            GlobalNotifications.remove();
-            $("#globalNotificationsSub").css("color", inactiveColor);
-            console.log('Unsubscribed!');
-        } else {
-            GlobalNotifications.set(true);
+    // console.dir(userEntityNotificationsExists);
 
-            $("#globalNotificationsSub").css("color", activeColor);
-            console.log('Subscribed!');
+    var GlobalNotifications = DB.child("users/"+userUuid+"/entityNotifications/"+activeEntity.entity+"/"+activeEntity.uid+"/globalNotifications");
 
-        }
+    if (userEntityNotificationsExists)
+    {
+
+      GlobalNotifications.remove();
+      $("#globalNotificationsSub").css("color", inactiveColor);
+      console.log('Unsubscribed!');
+    } else {
+      GlobalNotifications.set(true);
+
+      $("#globalNotificationsSub").css("color", activeColor);
+      console.log('Subscribed!');
+
     }
+  }
 }
 
 
 function pushNotification(EntityData, entityType) {
 
-    if (!Notification) {
-        alert('Desktop notifications not available in your browser. Try Chromium.');
-        return;
-    }
+  if (!Notification) {
+    alert('Desktop notifications not available in your browser. Try Chromium.');
+    return;
+  }
 
-    if (Notification.permission !== "granted")
-        Notification.requestPermission(EntityData);
-    else {
-        console.log(EntityData.val(), entityType );
-        var notification = new Notification(EntityData.val().title, {
-            icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
-            body: EntityData.val().description
-        });
+  if (Notification.permission !== "granted")
+    Notification.requestPermission(EntityData);
+  else {
+    console.log(EntityData.val(), entityType );
+    var notification = new Notification(EntityData.val().title, {
+      icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+      body: EntityData.val().description
+    });
 
-        notification.onclick = function () {
-            switch (entityType){
-                case "groups": showGroup(EntityData.key); break;
-                case "topics": showTopic(EntityData.key); break;
-                case "questions": showQuestion(EntityData.key); break;
-                // for a later use
-                // case "options": showOptionInfo(EntityData.key); break;
-            }
-        };
-    }
+    notification.onclick = function () {
+      switch (entityType){
+        case "groups": showGroup(EntityData.key); break;
+        case "topics": showTopic(EntityData.key); break;
+        case "questions": showQuestion(EntityData.key); break;
+          // for a later use
+          // case "options": showOptionInfo(EntityData.key); break;
+      }
+    };
+  }
 }
 
 function sendOwnerCall() {
-    
-    DB.ref("/"+activeEntity.entity+"/"+activeEntity.uid+"/ownerNotifications").push($("#callBox").val());
-    
-    
-    
+
+  DB.ref("/"+activeEntity.entity+"/"+activeEntity.uid+"/ownerNotifications").push($("#callBox").val());
+
+
+
 }
 
 function groupOwnerShoutout() {
-    
 
 
-    pushNotification(EntityData, entityType)
+
+  pushNotification(EntityData, entityType)
 }
 
 
